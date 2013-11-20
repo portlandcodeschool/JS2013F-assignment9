67e282bb9104e3c98ae8f695c20aa370d0a5732e
David Manning <dlmanning@me.com>
2013-11-19 18:34:05 -0800

    initial commit

---
+++ yourname/solutions/arrays.js
@@ -0,0 +1,18 @@
+module.exports.insertAt = function(arr, index, item) {
+
+// This function doesn't need to return anything, but rather modify
+// the passed array in place.
+  
+}
+
+module.exports.alphabetSorter = function(a, b) {
+  // This is the comparator function that will be passed
+  // into array.prototype.sort(). It should return a negative number
+  // if a comes before b alphabetically, a positive number if b comes
+  // before a and 0 if there's a tie
+
+}
+
+module.exports.itsInThere = function(arr, item) {
+  
+}
